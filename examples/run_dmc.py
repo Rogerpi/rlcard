ed3303914b57c4284aca4a3131b688a0fc92064b
Roger Pi Roig <roger.piroig@gmail.com>
2024-10-02 09:46:21 +0200

    added butifarra to dmc

--- examples/run_dmc.py
+++ examples/run_dmc.py
@@ -43,7 +43,8 @@ if __name__ == '__main__':
             'mahjong',
             'no-limit-holdem',
             'uno',
-            'gin-rummy'
+            'gin-rummy',
+            'butifarra',
         ],
     )
     parser.add_argument(
